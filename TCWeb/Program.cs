d6c4a23cd24d727f4d7121c44144fbed45a483eb
Sergey Svetlakov <ssvetlakov@getcrm.ru>
2020-03-31 08:44:19 +1000

    Change port to 5002

--- TCWeb/Program.cs
+++ TCWeb/Program.cs
@@ -20,7 +20,7 @@ namespace TCWeb
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://localhost:5002"); ;
                 });
     }
 }
